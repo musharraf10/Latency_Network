cb99d26585e80c6b2ee9024023bf4367bfc43f28
musharraf10 <skmusharaf00@gmail.com>
2025-09-09 23:31:30 +0530

    Updated Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,6 @@ import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
 export const metadata: Metadata = {
   title: "Latency Topology Visualizer",
   description:
